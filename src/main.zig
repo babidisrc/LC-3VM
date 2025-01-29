28089f0b9aea4fee13c7eb90cb5366f6733a7de3
babidisrc <thaissa.linguika@gmail.com>
2025-01-29 17:35:12 -0300

    add() implemented

--- src/main.zig
+++ src/main.zig
@@ -4,28 +4,28 @@ const MEMORY_MAX = 1 << 16; // 65536
 // store the memory in an array
 var memory: [MEMORY_MAX]u16 = std.mem.zeroes([MEMORY_MAX]u16);
 
-const Registers = enum { R0, R1, R2, R3, R4, R5, R6, R7, R_PC, R_COND, R_COUNT };
+const Registers = enum(usize) { R0, R1, R2, R3, R4, R5, R6, R7, R_PC, R_COND, R_COUNT };
 
 // store the registers in an array
-var reg: [Registers.R_COUNT]u16 = std.mem.zeroes([Registers.R_COUNT]u16);
+var reg: [@intFromEnum(Registers.R_COUNT)]u16 = std.mem.zeroes([@intFromEnum(Registers.R_COUNT)]u16);
 
-const Opcodes = enum {
-    OP_BR, // branch
-    OP_ADD, // add
-    OP_LD, // load
-    OP_ST, // store
-    OP_JSR, // jump register
-    OP_AND, // bitwise and
-    OP_LDR, // load register
-    OP_STR, // store register
-    OP_RTI, // unused
-    OP_NOT, // bitwise not
-    OP_LDI, // load indirect
-    OP_STI, // store indirect
-    OP_JMP, // jump
-    OP_RES, // reserved (unused)
-    OP_LEA, // load effective address
-    OP_TRAP, // execute trap
+const Opcodes = enum(usize) {
+    OP_BR = 0x0, // branch
+    OP_ADD = 0x1, // add
+    OP_LD = 0x2, // load
+    OP_ST = 0x3, // store
+    OP_JSR = 0x4, // jump register
+    OP_AND = 0x5, // bitwise and
+    OP_LDR = 0x6, // load register
+    OP_STR = 0x7, // store register
+    OP_RTI = 0x8, // unused
+    OP_NOT = 0x9, // bitwise not
+    OP_LDI = 0xA, // load indirect
+    OP_STI = 0xB, // store indirect
+    OP_JMP = 0xC, // jump
+    OP_RES = 0xD, // reserved (unused)
+    OP_LEA = 0xE, // load effective address
+    OP_TRAP = 0xF, // execute trap
 };
 
 const Flags = struct {
@@ -34,34 +34,84 @@ const Flags = struct {
     pub const FL_NEG: u8 = 1 << 2; // 4
 };
 
-pub fn main() void {
-    reg[Registers.R_COUNT] = Flags.FL_ZRO;
+fn mem_read(address: u16) u16 {
+    return memory[address];
+}
 
-    reg[Registers.R_PC] = 0x3000;
+fn mem_write(address: u16, val: u16) u16 {
+    memory[address] = val;
+}
 
-    const running: u32 = 1;
-    while (running) {
-        var instr: u16 = mem_read(reg[R_PC]++);
-        var op: u16 = instr >> 12;
+fn update_flags(r: Registers) void {
+    if (reg[r] == 0) {
+        reg[@intFromEnum(Registers.R_COUNT)] = Flags.FL_ZRO; // the value in r is zero
+    } else if (reg[r] >> 15) {
+        reg[@intFromEnum(Registers.R_COUNT)] = Flags.FL_NEG; // the value in r is z negative number
+    } else reg[@intFromEnum(Registers.R_COUNT)] = Flags.FL_POS;
+}
+
+fn signExtend(x: u16, bit_count: u16) u16 {
+    if (((x >> @intCast(bit_count - 1)) & 1) == 1) { // if the bth bit of n is 1 (number is negative)
+        return x | (@as(u16, 0xFFFF) << @intCast(bit_count)); // fill up with 1s the remaining bits (15)
+    }
+    return x; // else return the number as it is
+}
+
+// functions to extract bits
+fn FIMM(comptime i: u16) u16 {
+    return (i >> 5) & 1;
+}
+
+fn DR(comptime i: u16) u16 { // Destination Register
+    return (i >> 9) & 0x7;
+}
+
+fn SR1(comptime i: u16) u16 { // Source Register 1
+    return (i >> 6) & 0x7;
+}
+
+fn SR2(comptime i: u16) u16 { // Source Register 2
+    return i & 0x7;
+}
+
+fn IMM(comptime i: u16) u16 {
+    return i & 0x1F;
+}
+
+// ADD
+fn add(i: u16) void {
+    if (FIMM(i) == 1) { // if the 5th bit is 1
+        reg[DR(i)] = reg[SR1(i)] + signExtend(i); // we sign extend IMM5 and we add it to SR1 (add2)
+    } else {
+        reg[DR(i)] = reg[SR1(i)] + reg[SR2(i)]; // else we add the value of SR2 to SR1 (add1)
     }
 
-    switch (op) {
-        Opcodes.OP_ADD => std.debug.print("OP_ADD\n", .{}),
-        Opcodes.OP_AND => std.debug.print("OP_AND\n", .{}),
-        Opcodes.OP_NOT => std.debug.print("OP_NOT\n", .{}),
-        Opcodes.OP_BR => std.debug.print("OP_BR\n", .{}),
-        Opcodes.OP_JMP => std.debug.print("OP_JMP\n", .{}),
-        Opcodes.OP_JSR => std.debug.print("OP_JSR\n", .{}),
-        Opcodes.OP_LD => std.debug.print("OP_LD\n", .{}),
-        Opcodes.OP_LDI => std.debug.print("OP_LDI\n", .{}),
-        Opcodes.OP_LDR => std.debug.print("OP_LDR\n", .{}),
-        Opcodes.OP_LEA => std.debug.print("OP_LEA\n", .{}),
-        Opcodes.OP_ST => std.debug.print("OP_ST\n", .{}),
-        Opcodes.OP_STI => std.debug.print("OP_STI\n", .{}),
-        Opcodes.OP_STR => std.debug.print("OP_STR\n", .{}),
-        Opcodes.OP_TRAP => std.debug.print("OP_TRAP\n", .{}),
-        Opcodes.OP_RES => std.debug.print("OP_RES\n", .{}),
-        Opcodes.OP_RTI => std.debug.print("OP_RTI\n", .{}),
-        else => std.debug.print("BAD CODE\n", .{}),
+    update_flags(DR(i));
+}
+
+const Handler = *const fn () void;
+
+const jump_table = [16]Handler{ br, add, ld, st, jsr, andp, ldr, str, rti, not, ldi, sti, jmp, res, lea, trap };
+
+pub fn main() void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    var args = try std.process.argsWithAllocator(allocator);
+    defer args.deinit();
+
+    if (args.len < 2) {
+        std.debug.print("lc3 [image-file1] ...\n", .{});
+        return;
+    }
+
+    reg[@intFromEnum(Registers.R_COND)] = Flags.FL_ZRO;
+
+    reg[@intFromEnum(Registers.R_PC)] = 0x3000; // PC START
+
+    var running: bool = true;
+    while (running) {
+        running = false;
     }
 }
